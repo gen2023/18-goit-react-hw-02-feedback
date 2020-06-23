4be4d023621c06b9e97dced749844fe76f17e77b
Евгений <evgenij.romanko@neoseo.com.ua>
2020-06-23 23:16:58 +0300

    add build

---
+++ build/precache-manifest.223905dc3d7bd97943afe013595f0d93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7d8ef7680ff1043eb206a53ecb82b54",
+    "url": "/18-goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "511791baac7893430918",
+    "url": "/18-goit-react-hw-02-feedback/static/css/2.4b502e0b.chunk.css"
+  },
+  {
+    "revision": "4a7115c90b7f3e2243d9",
+    "url": "/18-goit-react-hw-02-feedback/static/css/main.fd6ae669.chunk.css"
+  },
+  {
+    "revision": "511791baac7893430918",
+    "url": "/18-goit-react-hw-02-feedback/static/js/2.dd0c82a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/18-goit-react-hw-02-feedback/static/js/2.dd0c82a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a7115c90b7f3e2243d9",
+    "url": "/18-goit-react-hw-02-feedback/static/js/main.2ec54bd5.chunk.js"
+  },
+  {
+    "revision": "380c0638019ec3027949",
+    "url": "/18-goit-react-hw-02-feedback/static/js/runtime-main.eca7714d.js"
+  }
+]);
